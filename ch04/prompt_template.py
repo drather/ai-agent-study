4905b38bdad637faba7e7ecc7a863387e1e15654
kks <drather@naver.com>
2026-01-06 23:24:00 +0900

    사용자 리뷰에 대한 평가 기능

---
+++ ch04/prompt_template.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+prompt_template = "이 음식 리뷰 '{review}' 에 대해 '{rating1}' 점부터  '{rating2}' 점까지 평가를 해주세요"
+
+prompt = PromptTemplate(
+    input_variables=["review", "rating1", "rating2"], template=prompt_template
+)
+
+openai = ChatOpenAI(model="gpt-3.5-turbo",api_key=OPENAI_API_KEY, temperature=0.7)
+
+chain = prompt | openai | StrOutputParser()
+
+print(f"prompt: {prompt}")
+
+try:
+    response = chain.invoke({
+        "review": "맛은 있었지만 배달 포장이 부족하여 아쉬웠습니다",
+        "rating1": "1",
+        "rating2": "5"
+    })
+    print(f"평가 결과: {response}")
+except Exception as e:
+    print(f"error: {e}")
+
+
