f0784cb44ee4deb8fdff0827e94959c60f508330
kks <drather@naver.com>
2026-01-12 23:22:43 +0900

    ch07 벡터 DB 및 검색기 구현

--- ch07/main.py
+++ ch07/main.py
@@ -1,8 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader
-from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from langchain_chroma import Chroma
+from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 load_dotenv()
 
 loader = PyPDFLoader("unsu.pdf")
@@ -15,11 +17,24 @@ text_spliter = RecursiveCharacterTextSplitter(
     is_separator_regex=False
 )
 
+texts = text_spliter.split_documents(pages)
+
 embeddings_model = OpenAIEmbeddings(
-    model="text-embeddings-3-large",
+    model="text-embedding-3-large",
     # With the 'text-embedding-3' class
     # of the models, you can specify the size of the embeddings you want returned.
     dimensions=1024
 )
 
+db = Chroma.from_documents(texts, embeddings_model)
 
+question = "아내가 먹고 싶어하는 음식은 뭐야?"
+llm = ChatOpenAI(temperature=0)
+
+retriever_from_llm = MultiQueryRetriever.from_llm(retriever=db.as_retriever(), llm=llm)
+
+docs = retriever_from_llm.invoke(question)
+print(len(docs))
+
+for doc in docs:
+    print(doc)
