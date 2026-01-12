3e791f95ed4a744954a182da8fef7017e4b07923
kks <drather@naver.com>
2026-01-12 23:43:23 +0900

    ch07 답변 생성기 구현

--- ch07/main.py
+++ ch07/main.py
@@ -1,9 +1,18 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_classic.retrievers.multi_query import MultiQueryRetriever
 from langchain_openai import ChatOpenAI
+import langchainhub as hub
+from langchain_core.output_parsers import StrOutputParser
+
+import json
+
+from langsmith import Client
+from langchain_core.load import loads
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -28,13 +37,28 @@ embeddings_model = OpenAIEmbeddings(
 
 db = Chroma.from_documents(texts, embeddings_model)
 
-question = "아내가 먹고 싶어하는 음식은 뭐야?"
 llm = ChatOpenAI(temperature=0)
 
 retriever_from_llm = MultiQueryRetriever.from_llm(retriever=db.as_retriever(), llm=llm)
 
-docs = retriever_from_llm.invoke(question)
-print(len(docs))
 
-for doc in docs:
-    print(doc)
+client = Client()
+
+# hub.pull() 메서드가 동작하지 않아, 수정하여 진행
+prompt = client.pull_prompt("rlm/rag-prompt")
+
+# 생성기
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever_from_llm | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+# Question
+result = rag_chain.invoke("아내가 먹고싶어하는 음식이 뭐야?")
+print(result)
+
