67b189b2fcfdf5c7b359f4a66c90e2c663ce912a
kks <drather@naver.com>
2026-01-12 23:14:02 +0900

    ch07 chunk spliter, 임베딩 모델 구현

--- ch07/main.py
+++ ch07/main.py
@@ -1,6 +1,25 @@
 from langchain_community.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
 
 loader = PyPDFLoader("unsu.pdf")
 pages = loader.load_and_split()
 
-print(pages[0])
+text_spliter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=20,
+    length_function=len,
+    is_separator_regex=False
+)
+
+embeddings_model = OpenAIEmbeddings(
+    model="text-embeddings-3-large",
+    # With the 'text-embedding-3' class
+    # of the models, you can specify the size of the embeddings you want returned.
+    dimensions=1024
+)
+
+
