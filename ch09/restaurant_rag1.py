853e9151bcf98e641f27afb1736ebcf1a8b6c1ee
kks <drather@naver.com>
2026-01-22 00:19:01 +0900

    ch09 환경변수 로딩

---
+++ ch09/restaurant_rag1.py
@@ -0,0 +1,10 @@
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import TextLoader
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
