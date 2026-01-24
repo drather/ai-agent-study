940601ce58ca2c91a66474d46e51c5c180607b62
kks <drather@naver.com>
2026-01-24 13:23:46 +0900

    ch09 document 를 chunk 단위로 분할

--- ch09/restaurant_rag1.py
+++ ch09/restaurant_rag1.py
@@ -5,6 +5,9 @@ from langchain_community.document_loaders import TextLoader
 import os
 
 from dotenv import load_dotenv
+
+from ch07.main import text_spliter
+
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
@@ -15,3 +18,8 @@ restaurant_faiss= os.path.join(current_dir, "restaurant-faiss")
 loader = TextLoader(f'{current_dir}/restaurant.txt')
 
 documents = loader.load()
+
+text_spliter = CharacterTextSplitter(chunk_size=300, chunk_overlap=50)
+
+docs = text_spliter.split_documents(documents)
+
