ff74a8bfdd868e55c6ab0e8747d9faf5f64186fa
kks <drather@naver.com>
2026-02-03 06:57:33 +0900

    ch13 파일 업로드 및 splitter

--- ch12-13/rag_advanced_streamlit.py
+++ ch12-13/rag_advanced_streamlit.py
@@ -30,7 +30,7 @@ model_choice = st.selectbox(
 )
 
 # 파일 업로드
-upploaded_file = st.file_uploader("PDF 파일을 업로드하세요. ", type=["pdf"])
+uploaded_file = st.file_uploader("PDF 파일을 업로드하세요. ", type=["pdf"])
 st.wrtie("---")
 
 # PDF 를 문서로 변환하는 함수
@@ -46,3 +46,17 @@ def pdf_to_document(uploaded_file):
 # 문서를 포매팅
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+
+
+# 파일 업로드 확인
+if uploaded_file is not None:
+    pages = pdf_to_document(uploaded_file)
+
+    # 문서를 청크로 분할
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+
+    splits = text_splitter.split_documents(pages)
+
