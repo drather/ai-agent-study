a4caf647f8a53da6d7dcbddca08a5dcd165c51fa
kks <drather@naver.com>
2026-02-12 23:31:28 +0900

    ch15 import 작성

---
+++ ch15/langchain-fashion-multimodal-script.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+import chromadb
+from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
+from chromadb.utils.data_loaders import ImageLoader
+
