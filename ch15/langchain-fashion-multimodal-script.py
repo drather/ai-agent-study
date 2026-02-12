0ac4de279da8e1e1fafb1631413f9efb2ca2cb9d
kks <drather@naver.com>
2026-02-12 23:32:57 +0900

    ch15 import 추가

--- ch15/langchain-fashion-multimodal-script.py
+++ ch15/langchain-fashion-multimodal-script.py
@@ -3,4 +3,8 @@ from dotenv import load_dotenv
 import chromadb
 from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
 from chromadb.utils.data_loaders import ImageLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
 
