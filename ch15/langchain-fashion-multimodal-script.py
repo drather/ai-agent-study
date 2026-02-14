f73e752e905d7ccfbed70d61a1bfd7dfe0f5e756
kks <drather@naver.com>
2026-02-15 00:51:05 +0900

    ch15 vector db insert, 번역/시각정보처리 chain 구현

--- ch15/langchain-fashion-multimodal-script.py
+++ ch15/langchain-fashion-multimodal-script.py
@@ -34,3 +34,70 @@ def save_images(dataset, dataset_folder, num_images=500):
     print(f"saved {num_images} images to {dataset_folder}")
 
 
+def setup_chroma_db():
+    vdb_path = os.path.join(SCRIPT_DIR, "img_vdb")
+
+    chroma_client = chromadb.PersistentClient(path=vdb_path)
+
+    image_loader = ImageLoader()
+
+    CLIP = OpenCLIPEmbeddingFunction()
+
+    image_vdb = chroma_client.get_or_create_collention(name="image", embedding_function=CLIP, data_loader=image_loader)
+    return image_vdb
+
+def add_images_to_db(image_vdb, dataset_folder) :
+    ids = []
+    uris = []
+
+    for i, filename in enumerate(sorted(os.listdir(dataset_folder))):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            file_path = os.path.join(dataset_folder, filename)
+            ids.append(str(i))
+            uris.append(file_path)
+        image_vdb.add(ids=ids, uris=uris)
+        print("이미지가 데이터베이스에 추가되었습니다.")
+
+def query_db(image_vdb, query, results=2):
+    return image_vdb.query(
+        query_texts=[query],
+        n_results=results,
+        includ=['uris', 'distances']
+    )
+
+def print_results(results):
+    for idx, uri in enumerate(results['uris'][0]):
+        print(f"ID: {results['ids'][0][idx]}")
+        print(f"Distances: {results['distances'][0][idx]}")
+        print(f"Path: {uri}")
+        print("\n")
+
+def translate(text, target_lang):
+    translation_model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0)
+
+    translation_prompt = ChatPromptTemplate.from_messages([
+        ("system", f"You are a translator. Translate the following text: {text} to {target_lang}", ),
+        ("user", "{text")
+    ])
+
+    translation_chain = translation_prompt | translation_model | StrOutputParser()
+
+    return translation_chain.invoke({"text": text})
+
+def setup_vision_chain():
+    # GPT-4 모델을 사용하여 시각적 정보를 처리 gpt-4o or gpt-4o-mini 모델선택
+    gpt4 = ChatOpenAI(model="gpt-4o-mini", temperature=0.0)
+    parser = StrOutputParser()
+
+    # 당신은 유용한 패션 및 스타일링 보조자입니다. 제공된 이미지 부분을 직접 참조하여 주어진 이미지 컨텍스트를 사용하여 사용자의 질문에 답합니다. 좀 더 대화적인 분위기를 유지하고 목록을 너무 많이 만들지 마십시오. 하이라이트, 강조, 구조를 위해 마크다운 형식을 사용하세요.
+    image_prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful fashion and styling assistant. Answer the user's question using the given image context with direct references to parts of the images provided. Maintain a more conversational tone, don't make too many lists. Use markdown formatting for highlights, emphasis, and structure."),
+        ("user", [
+            {"type": "text", "text": "What are some ideas for styling {user_query}"},
+            {"type": "image_url", "image_url": "data:image/jpeg;base64,{image_data_1}"},
+            {"type": "image_url", "image_url": "data:image/jpeg;base64,{image_data_2}"},
+        ]),
+    ])
+    # 프롬프트, 모델, 파서 체인을 반환
+    return image_prompt | gpt4 | parser
+
